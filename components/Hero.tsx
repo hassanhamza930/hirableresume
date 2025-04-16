c05b18a97f708cdc2e6cbd3a2675c5f0f900d453
Hamza Hassan <hassanhamza930@gmail.com>
2025-04-16 13:07:12 +0500

    Minor UI Ux

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -72,7 +72,7 @@ export default function Hero() {
                 <img src="chrome.gif" className="w-full h-full absolute z-0 object-cover object-center" />
                 <div className="w-full h-full absolute z-10 object-cover object-center bg-zinc-800/50 backdrop-blur-2xl backdrop-brightness-[1.0] backdrop-contrast-[3.0] backdrop-saturate-200" />
 
-                <div className="relative z-10  w-full md:w-auto md:h-96 rounded-xl overflow-hidden flex flex-col md:flex-row justify-center items-center bg-black">
+                <div className="relative z-10  w-full md:w-auto md:h-[450px] rounded-xl overflow-hidden flex flex-col md:flex-row justify-center items-center bg-black">
                     <video src="/hero.mp4" autoPlay loop muted className="h-full w-full object-cover rounded-none"></video>
                     <video src="/hero2.mp4" autoPlay loop muted className="h-full w-full object-cover rounded-xl"></video>
                 </div>
