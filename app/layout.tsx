86d46b479ba9cc746e081d356e0f9180cb4dc7ca
Hamza Hassan <hassanhamza930@gmail.com>
2025-04-20 18:19:10 +0500

    Clarity Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { RootWrapper } from './components/RootWrapper';
+import Script from "next/script";
 
 export const viewport = {
   width: 'device-width',
@@ -98,6 +99,17 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&family=Geist:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Special+Gothic+Expanded+One&display=swap"
           rel="stylesheet"
         />
+        <Script type="text/javascript">
+          {
+            `
+           (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "r746c0yon6");
+          `
+          }
+        </Script>
       </head>
       <body
         className={`antialiased bg-zinc-950`}
